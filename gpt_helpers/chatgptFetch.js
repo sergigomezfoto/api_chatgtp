b1b963795075af711a778bcce5a06f152ee3c872
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-08-06 15:52:05 +0200

    fen servir el objecte oficial en lloc de fetch

--- gpt_helpers/chatgptFetch.js
+++ gpt_helpers/chatgptFetch.js
@@ -1,32 +1,24 @@
 
+const { Configuration, OpenAIApi } = require('openai');
 require("dotenv").config();
-const fetch = require("node-fetch");
-const KEY = process.env.OPENAI_KEY;
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_KEY
+});
+const openai = new OpenAIApi(config);
 
 async function fetchFromOpenAI(messages) {
-    const data = {
-      model: "gpt-3.5-turbo",
-      messages: messages,
-      temperature: 0.8,
-    };
-  
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      credentials: 'include',
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${KEY}`,
-      },
-      body: JSON.stringify(data),
-    });
-  
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error('Error from OpenAI:', errorData);
+  try {
+      const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: messages,
+          temperature: 0.8,
+      });
+
+      return response.data;
+  } catch (error) {
+      console.error('Error from OpenAI:', error.response?.data);
       throw new Error('Failed to fetch from OpenAI');
   }
-  
-    return await response.json();
-  }
-
+}
 module.exports = { fetchFromOpenAI };
